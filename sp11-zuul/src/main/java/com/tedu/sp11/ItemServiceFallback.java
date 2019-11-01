ec7831e575b92addb59b52c9ae319a69862a9e23
000 <000@192.168.14.63>
2019-11-01 15:41:37 +0800

    sp-08/09/10/11

---
+++ sp11-zuul/src/main/java/com/tedu/sp11/ItemServiceFallback.java
@@ -0,0 +1,73 @@
+package com.tedu.sp11;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import com.tedu.web.util.JsonResult;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ItemServiceFallback implements FallbackProvider {
+
+	@Override
+	public String getRoute() {
+		/**
+		 * 	路由规则:返回一个service-id当前降级类,只对指定的微服务有效
+		 */
+		//当执行item-service失败，
+	    //应用当前这个降级类
+		return "item-service";
+		
+		//return null;	对所有服务有效
+		//return "";	对所有服务有效
+	}
+
+	@Override
+	public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+		//降级响应,降级响应的数据,封装成一个response对象
+		return reponse();
+	}
+
+	private ClientHttpResponse reponse() {
+		return new ClientHttpResponse() {
+            //下面三个方法都是协议号
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.OK.value();
+            }
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.OK.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+            	log.info("fallback body");
+            	String s = JsonResult.err().msg("后台服务错误").toString();
+                return new ByteArrayInputStream(s.getBytes("UTF-8"));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+	}
+
+}
